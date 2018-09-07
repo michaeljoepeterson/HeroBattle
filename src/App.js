d706b79239260af1ae3b53033dc5b06478676b74
Michael Peterson <michaeljoepeterson@gmail.com>
2018-09-07 10:27:44 -0600

    added landing page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Route, withRouter} from 'react-router-dom';
 import LoginForm from './components/login-form';
-
+import LandingPage from './components/landing-page';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <LoginForm />
+        <Route exact path="/" component={LandingPage} />
       </div>
     );
   }
---
+++ src/components/landing-page.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {Link, Redirect} from 'react-router-dom';
+
+import LoginForm from './login-form';
+
+export function LandingPage(props){
+	return (
+        <div className="home">
+            <h2>Welcome</h2>
+            <LoginForm />
+        </div>
+    );
+} 
+
+const mapStateToProps = state => ({
+    loggedIn: state.auth.currentUser !== null
+});
+
+export default connect(mapStateToProps)(LandingPage);
