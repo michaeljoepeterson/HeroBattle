080296fcba05e8abc69b6d0b3240a882b5a96e71
Michael Peterson <michaeljoepeterson@gmail.com>
2018-10-01 19:06:23 -0600

    leaderboard component working

--- src/components/leaderboard-page.js
+++ src/components/leaderboard-page.js
@@ -13,6 +13,21 @@ export class LeaderboardPage extends React.Component{
     }
 	render(){
 		console.log("here are the scores ",this.props.scores);
+		let scoreData = [];
+		try{
+			if(this.props.scores){
+				scoreData = this.props.scores.map((score,index) => (<tr key={score.username}>
+						<td>{index + 1}</td>
+						<td>{score.username}</td>
+						<td>{score.wins}</td>
+						<td>{score.winRate}</td>
+						<td>{score.matches}</td>
+					</tr>))
+			}
+		}
+		catch(err){
+			scoreData = null;
+		}
 		return(
 			<div>
 				<Navbar />
@@ -21,11 +36,13 @@ export class LeaderboardPage extends React.Component{
 					<table className="defaultTable">
 						<tbody>
 							<tr>
+								<th>Rank</th>
 								<th>User</th>
 								<th>Wins</th>
 								<th>Matches</th>
 								<th>Win Rate</th>
 							</tr>
+							{scoreData}
 						</tbody>
 					</table>
 				</div>
